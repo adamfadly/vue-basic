9f1900e70caf52ff5f802332e590abf9477c041d
Adam Fadly <adamfadly11@gmail.com>
2019-11-25 07:09:22 +0700

    vue props

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar />
+    <AllFriends :friends="friends" @delete="deleteFriend" />
+    <OnlineFriends :friends="friends" />
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+import AllFriends from './AllFriends'
+import OnlineFriends from './OnlineFriends'
+
+
+
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    AllFriends,
+    OnlineFriends
+  },
+
+  data () {
+    return {
+     title: 'I will succes in software Development',
+     friends: [
+        { name: 'Mario', online: true },
+        { name: 'Luigi', online: false },
+        { name: 'Tood', online: true },
+        { name: 'Bowser', online: false }
+      ],
+    }
+  },
+  methods: {
+    deleteFriend(payload) {
+      console.log(payload)
+      this.friends = this.friends.filter( friend =>{
+        return friend.name !== payload.name
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app{
+  padding: 100px 200px;
+}
+h1 {
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
---
+++ src/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="main-nav">
+        <h1>Menu</h1>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+   name: 'Navbar',
+   data() {
+       return {
+
+       }
+   } 
+}
+</script>
+
+<style scooped>
+
+nav {
+    text-align: center;
+}
+
+nav li {
+    display: inline-block;
+    list-style-type: none;
+    margin: 0;
+}
+
+ul{
+    padding: 0;
+}
+</style>
