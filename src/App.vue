afeb0f4422e02cce18d43c1e2d2710da51181c1a
adam fadly <adamfadly11@gmail.com>
2019-11-27 19:00:19 +0700

    api

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,28 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-    <AllFriends :friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends" />
+    <Blogs />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar'
-import AllFriends from './AllFriends'
-import OnlineFriends from './OnlineFriends'
-
+import Blogs from './Blogs'
 
 
 export default {
   name: 'app',
   components: {
     Navbar,
-    AllFriends,
-    OnlineFriends
+    Blogs
   },
 
   data () {
     return {
      title: 'I will succes in software Development',
-     friends: [
-        { name: 'Mario', online: true },
-        { name: 'Luigi', online: false },
-        { name: 'Tood', online: true },
-        { name: 'Bowser', online: false }
-      ],
     }
   },
   methods: {
-    deleteFriend(payload) {
-      console.log(payload)
-      this.friends = this.friends.filter( friend =>{
-        return friend.name !== payload.name
-      })
-    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Global Filter
+Vue.filter ('snippet', val => {
+  if(!val || typeof(val) != 'string') return ''
+  val = val.slice(0,50)
+  return val
+})
+
+
 new Vue({
   el: '#app',
   render: h => h(App)
