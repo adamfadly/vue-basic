93d31f8558464b211188c5534f3e3a53051ea242
adam fadly <adamfadly11@gmail.com>
2019-11-27 10:28:31 +0700

    deleted component Allfirends & Onlinefriends

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,12 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-    <AllFriends :friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar'
-import AllFriends from './AllFriends'
-import OnlineFriends from './OnlineFriends'
+
 
 
 
@@ -18,28 +15,15 @@ export default {
   name: 'app',
   components: {
     Navbar,
-    AllFriends,
-    OnlineFriends
   },
 
   data () {
     return {
      title: 'I will succes in software Development',
-     friends: [
-        { name: 'Mario', online: true },
-        { name: 'Luigi', online: false },
-        { name: 'Tood', online: true },
-        { name: 'Bowser', online: false }
-      ],
     }
   },
   methods: {
-    deleteFriend(payload) {
-      console.log(payload)
-      this.friends = this.friends.filter( friend =>{
-        return friend.name !== payload.name
-      })
-    }
+
   }
 }
 </script>
